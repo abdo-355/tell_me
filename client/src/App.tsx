5b3ea59bb05bf0ce07d26bb117f5e35ab34c2385
abdo-355 <abdoluxor366@gmail.com>
2023-01-08 16:16:14 +0200

    adding tailwind and setting it up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div >
-      <h1>TELLME app</h1>
+    <div className="bg-green-400 w-full">
+      <h1 className="text-green-50 font-black">TELLME app</h1>
     </div>
   );
-}
+};
 
 export default App;
