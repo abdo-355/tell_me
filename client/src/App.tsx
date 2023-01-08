a567a7e3100296c832302e5a517835a03eb3132d
abdo-355 <abdoluxor366@gmail.com>
2023-01-08 23:31:27 +0200

    add the header and update the favicon icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div className="bg-green-400 w-full">
-      <h1 className="text-green-50 font-black">TELLME app</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
