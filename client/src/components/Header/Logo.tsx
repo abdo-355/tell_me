266a0a4dd20fdf926db570ec3e2f015fd2508863
abdo-355 <abdoluxor366@gmail.com>
2023-01-10 21:20:41 +0200

    enhance the header on small screens

--- client/src/components/Header/Logo.tsx
+++ client/src/components/Header/Logo.tsx
@@ -1,10 +1,21 @@
 import logo from "../../assets/logo-text.png";
+import logoOnly from "../../assets/logo-only.png";
 
 const Logo = () => {
   return (
     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-      <div className="flex flex-shrink-0 items-center bg-white px-3 py-2 rounded-full shadow-md hover:shadow-green-700 hover:scale-105 transition-all my-2">
-        <img className="block h-9 w-auto" src={logo} alt="website logo" />
+      <div className="flex flex-shrink-0 items-center bg-white px-2 sm:px-3 py-2 rounded-full shadow-md hover:shadow-green-700 hover:scale-105 transition-all my-2">
+        <img
+          className="hidden h-9 w-auto sm:block"
+          src={logo}
+          alt="website logo"
+        />
+        {/* for mobile devices */}
+        <img
+          className="block h-9 w-9 scale-125 rounded-full sm:hidden"
+          src={logoOnly}
+          alt="website logo"
+        />
       </div>
     </div>
   );
--- client/src/components/Header/NavButton.tsx
+++ client/src/components/Header/NavButton.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 const NavButton: FC<{ content: string }> = ({ content }) => {
   return (
     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-      <button className="font-sans text-white text-2xl bg-green-600 hover:bg-green-700 px-3 py-1 rounded-full border-black tracking-wider uppercase">
+      <button className="font-sans text-white text-2xl sm:bg-green-600 sm:hover:bg-green-700 px-0 sm:px-3 py-1 rounded-full sm:tracking-wider sm:uppercase">
         {content}
       </button>
     </div>
