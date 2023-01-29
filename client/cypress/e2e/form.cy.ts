2f979d5d0769b1312464e680d6b3b650e9389a67
abdo-355 <abdoluxor366@gmail.com>
2023-01-30 00:14:29 +0200

    add typescript support to our custom command

--- client/cypress/e2e/form.cy.ts
+++ client/cypress/e2e/form.cy.ts
@@ -42,7 +42,6 @@ describe("using forms", () => {
     });
 
     //the user logged in if the userId is in the local storage
-    //@ts-ignore
     cy.readLocalStorage("token").should("equal", token);
   });
 });
