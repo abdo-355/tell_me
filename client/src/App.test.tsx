ed13d5bd08e4ffe3132469ac1254b8562dd9a799
abdo-355 <abdoluxor366@gmail.com>
2023-01-30 02:01:34 +0200

    configure jest and write the first test

---
+++ client/src/App.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+
+describe("<App />", () => {
+  test("renders the home page for initial load", () => {
+    render(<App />, { wrapper: BrowserRouter });
+    const homeHeader = screen.getByRole("heading", {
+      name: /let them tell you/i,
+    });
+
+    expect(homeHeader).toBeInTheDocument();
+  });
+});
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,6 @@ const App = () => {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
       </Route>
-      {/* <Route path="/auth/*" element={<Form />} /> */}
     </Routes>
   );
 };
