7c0432e378d861fc9d5bcbc9d91f87c95d59a7b2
abdo-355 <abdoluxor366@gmail.com>
2023-01-30 22:39:21 +0200

    remove unwanted test Id

--- client/src/components/UI/ArrowLeft.tsx
+++ client/src/components/UI/ArrowLeft.tsx
@@ -7,7 +7,6 @@ interface Props {
 const ArrowLeft: FC<Props> = ({ styles }) => {
   return (
     <svg
-      data-testid="backArrow"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
