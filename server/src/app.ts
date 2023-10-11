a0ec9ddb7ba425b08183f4ce262dc094495b2c8c
abdo-355 <abdoluxor366@gmail.com>
2023-10-11 18:30:56 +0300

    fix cors v3

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 config();
 
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONT_END }));
+// app.use(cors({ origin: process.env.FRONT_END }));
 app.use(
   session({
     secret: process.env.EXPRESS_SESSION_SECRET,
@@ -21,6 +21,26 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use((req, res, next) => {
+  // fixing the cors problem
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_END);
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/api", (req, res) => {
   res.send("The server is working");
